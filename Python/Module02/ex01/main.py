2a1d2680f15eda790619be88b392f21d7eb9bd22
sergemad <sergekeita01@gmail.com>
2022-02-27 18:05:03 +0100

    args and kwargs

---
+++ Python/Module02/ex01/main.py
@@ -0,0 +1,31 @@
+def what_are_the_vars(*args,**kwargs):
+    for (kwarg,kval) in zip(kwargs,kwargs.values()):
+        print(f"{kwarg}: {kval}")
+    for i,arg in enumerate(args):
+        print(f"val_{i}: {arg}")
+
+class ObjectC(object):
+    def __init__(self):
+        pass
+
+def doom_printer(obj):
+    if obj is None:
+        print("ERROR")
+        print("end")
+        return
+    for attr in dir(obj):
+        if attr[0] != '_':
+            value = getattr(obj, attr)
+            print("{}: {}".format(attr, value))
+    print("end")
+if __name__ == "__main__":
+    obj = what_are_the_vars(7)
+    doom_printer(obj)
+    obj = what_are_the_vars("ft_lol", "Hi")
+    doom_printer(obj)
+    obj = what_are_the_vars()
+    doom_printer(obj)
+    obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, a=10, var_0="world")
+    doom_printer(obj)
