06aa9ec78a610e9650a15c0dfc84ca55cb84a0e7
sergemad <sergekeita01@gmail.com>
2022-03-01 15:54:28 +0100

    Typing

--- Python/Module02/ex00/ft_reduce.py
+++ Python/Module02/ex00/ft_reduce.py
@@ -1,10 +1,6 @@
-def ft_reduce(function_to_apply,list_of_inputs):
-    if isinstance(list_of_inputs,list):
-        r = list_of_inputs[0]
-        for i in range(1,len(list_of_inputs)):
-            r = function_to_apply(r,list_of_inputs[i])
-        
-        return r
+def ft_reduce(function_to_apply,list_of_inputs: list)-> list:
+    r = list_of_inputs[0]
+    for i in range(1,len(list_of_inputs)):
+        r = function_to_apply(r,list_of_inputs[i])
     
-    else:
-        print("ERROR : Bad parameter")
+    return r
